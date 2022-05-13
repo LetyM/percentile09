b80e112de6dbf7d6704262c73b8dbd70a073467b
Leticia Montero <leticia.montero@cbre.com>
2022-05-13 10:17:28 +0200

    feat: parquet/csv option sh

--- percentile90.py
+++ percentile90.py
@@ -6,7 +6,7 @@ import sys
 file = sys.argv[1]
 distance_column = sys.argv[2]
 
-df = pd.read_csv(file)
+df = pd.read_parquet(file)
 
 df_sorted = df.sort_values(by=distance_column)
 p = len(df) * 90 / 100
@@ -15,4 +15,5 @@ df_90 = df[df[distance_column] > df_sorted.iloc[ceil_p][distance_column]]
 
 df_90.to_csv('percentile90.csv')
 
-# python3 percentile90.py "C:\Users\Let\Documents\PythonScripts\yellow_tripdata_2022-01.csv" "trip_distance"
+# python3 percentile90.py https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2022-01.parquet "trip_distance"
+# pip install fsspec
--- percentile90.sh
+++ percentile90.sh
@@ -1,5 +1,19 @@
 # percentile90
-while true; do
+
+echo "Welcome to percentile90"
+read -p 'File extension [parquet/csv]: ' file_extension
+case $file_extension in 
+    'parquet' | 'csv')
     read -p 'File url: ' file_url
-    python3 percentile90.py
-done
+    read -p 'Distance column: ' distance_column
+    python3 percentile90.py $file_url $distance_column
+    echo "Process finished"
+
+    break
+    ;;
+    *)
+    echo "Invalid option"
+    break
+    ;;
+esac
+
