56cb85eafbfd2c1abd97695f72a128b8ef9742c8
Leticia Montero <leticia.montero@cbre.com>
2022-05-14 17:50:20 +0200

    refactor: output

--- percentile09.py
+++ percentile09.py
@@ -1,17 +1,23 @@
-# Percentile 90
+# Percentile 09
 import pandas as pd
 import numpy as np
 import sys
 
 file = sys.argv[1]
 distance_column = sys.argv[2]
-output_file = sys.argv[3]
+output_file_name = sys.argv[3]
 
+print('Reading file')
 df = pd.read_parquet(file)
 
+print('Sorting and calculating percentile 0.9')
 df_sorted = df.sort_values(by=distance_column)
 p = len(df) * 90 / 100
 ceil_p = int(np.ceil(p))
+
+print('Filtering by percentile')
 df_90 = df[df[distance_column] > df_sorted.iloc[ceil_p][distance_column]]
 
-df_90.to_csv(output_file)
+print('Writing result')
+df_90.to_csv(f"Output/{output_file_name}.csv")
+
--- percentile09.sh
+++ percentile09.sh
@@ -8,8 +8,8 @@ case $file_extension in
     'parquet' | 'csv')
     read -p 'File url: ' file_url
     read -p 'Distance column: ' distance_column
-    read -p 'Output file path:' output_file
-    python3 percentile90.py $file_url $distance_column $output_file
+    read -p 'Output file name:' output_file
+    python3 percentile09.py $file_url $distance_column $output_file
     echo "Process finished"
 
     break
